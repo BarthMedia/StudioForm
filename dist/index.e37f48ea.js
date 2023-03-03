0549a84778d558484518511e5e2488c652e7b1f4
Leon <email@leon-barth.com>
2023-03-03 10:07:06 +0100

    Initial pre-filled template commit 🚀

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2601,20 +2601,26 @@ var _regeneratorRuntime = require("regenerator-runtime");
 // Custom
 var _configJs = require("./config.js");
 const timeout = function(s) {
+    // Return
     return new Promise(function(_, reject) {
+        // Timeout
         setTimeout(function() {
+            // Create error
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
 };
 const getJson = async function(url) {
     try {
+        // Values
         const res = await Promise.race([
             fetch(url),
             timeout(_configJs.TIMEOUT_SEC)
         ]);
         const data = await res.json();
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`); // Sweet jezuz syntax. THIS IS AWESOME !!!
+        // Logic
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        // Return
         return data;
     } catch (err) {
         throw err;
